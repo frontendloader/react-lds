23634803ff1f438c99deeb77f567958f8848ad45
Carlo Zottmann <carlo@municode.de>
2017-02-15 12:06:55 +0100

    [NEW] Renders rows, fixes sorting column display, adds row selection

--- docs/src/app/pages/DataTable/ExampleAdvanced.js
+++ docs/src/app/pages/DataTable/ExampleAdvanced.js
@@ -3,25 +3,48 @@ import React from 'react';
 import { DataTableAdvanced, DataTableColumn } from 'react-lds';
 
 
+const data = [
+  {
+    id: 1,
+    col1: 'bork',
+    col2: 'hello',
+    col3: 123,
+    col4: 'pjqe e129 osfnoejfo oief oiwe',
+    moo: true,
+  },
+  {
+    id: 2,
+    col1: 'herp derp',
+    col2: 'hej hej',
+    col3: 246,
+    col4: 'osfnoejfo oief oiwe',
+    moo: false,
+  },
+];
+
 const ExampleAdvanced = () => (
   <DataTableAdvanced
     bordered
     fixed-layout
-    data={[]}
+    data={data}
     hasSelectableRows
-    onSorting={() => {}}
+    onSorting={(...rest) => console.log(rest)}
   >
     <DataTableColumn
-      dataKey="column1"
+      dataKey="col1"
       isResizable={false}
       isSortable
       title="Column 1"
     />
     <DataTableColumn
-      dataKey="column2"
+      dataKey="col2"
       isResizable
       isSortable={false}
       title="Column 2"
+    />
+    <DataTableColumn
+      dataKey="col4"
+      title="Column 4"
     />
   </DataTableAdvanced>
 );
